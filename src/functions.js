a038e411d5cd1b7703524c17cd40cd593fab57d3
Matteo C <35396314+Micorksen@users.noreply.github.com>
2022-02-05 17:01:00 +0100

    Create functions.js

---
+++ src/functions.js
@@ -0,0 +1,5 @@
+/*
+ * Never-Get-Rickrolled functions <https://github.com/Micorksen/Never-Get-Rickrolled>
+ * Copyright 2022 Micorksen <https://micorksen.eu.org>
+ * Available under MIT license <https://mths.be/mit>
+*/
