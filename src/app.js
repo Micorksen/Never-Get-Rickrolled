1fd5eaeaebea73b565dd0eba99d03e9c7e5385d6
Matteo C <35396314+Micorksen@users.noreply.github.com>
2022-02-05 09:12:33 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const xhr = new XMLHttpRequest();
-
 let links;
+
 xhr.onreadystatechange = (event) =>{
     event = event.currentTarget;
     if(event.readyState == 4 && event.status == 200){
         links = JSON.parse(event.responseText);
-        links.forEach((link) =>{
-            if(new RegExp(link).test(window.location.href) && !window.location.href.includes("#rickroll-allowed")){
-                console.log("--- Rickrolled detected ! ---");
-                window.location.replace(`${chrome.runtime.getURL("page.html")}?fullURL=${window.location.href}#${window.location.host}`);
-            }
-        });
+        result = links.filter((link) => new RegExp(link.replace("?", "\\?")).test(window.location.href) && !window.location.href.includes("#rickroll-allowed"));
+        
+        if(result.length > 0){
+            console.log("--- Rickrolled detected ! ---");
+            window.location.replace(`${chrome.runtime.getURL("page.html")}?fullURL=${window.location.href}#${window.location.host}`);
+        }
     }
 };
 
